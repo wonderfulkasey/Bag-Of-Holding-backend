ecb50552bf47791052bcd0095674295a1e731466
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 13:03:41 -0500

    spacing

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -1,4 +1,6 @@
 class Bag < ApplicationRecord
     belongs_to :character
     validates :title, :description, presence: true
+
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ApplicationRecord
     has_many :bags
     validates :name, presence :true
+
+    
 end
