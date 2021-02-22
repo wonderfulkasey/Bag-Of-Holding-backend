d72488e0e17cb7f9c6959e1d0a3927794dc5db07
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 14:35:18 -0500

    adding bag controller

--- app/controllers/api/v1/bags_controller.rb
+++ app/controllers/api/v1/bags_controller.rb
@@ -1,5 +1,26 @@
 class Api::V1::BagsController < ApplicationController
 
-    
+    def index
+       
+    end 
+
+    def create
+       
+    end
+
+    def show
+        
+    end 
+
+    def destroy
+       
+    end 
+
+
+    private 
+
+    def bag_params
+        params.require(:bag).permit(:character_id, :title, :description)
+    end
 
 end
