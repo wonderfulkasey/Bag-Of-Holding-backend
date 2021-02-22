e79f2f9b85074feaee5ee6dfde19a9c63a645a62
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 14:25:01 -0500

    adding to char controller params

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::CharactersController < ApplicationController
+
+    def index
+        
+    end 
+
+    def create
+        
+    end
+
+    def show
+
+    end 
+
+    def destroy
+
+    end 
+
+
+    private 
+
+    def character_params
+        params.require(:character).permit(:name)
+    end
+
 end
