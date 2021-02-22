abd53de13240766f03e98f598baafd70e0ad55f5
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 14:40:42 -0500

    set_character before action added

--- app/controllers/api/v1/bags_controller.rb
+++ app/controllers/api/v1/bags_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::BagsController < ApplicationController
+     
+    before_action :set_character
 
     def index
        
@@ -18,6 +20,10 @@ class Api::V1::BagsController < ApplicationController
 
 
     private 
+
+    def set_character
+        @character = Character.find(params[:character_id])
+    end
 
     def bag_params
         params.require(:bag).permit(:character_id, :title, :description)
