0423c21e8f977c194dec91c3ea2af12798fa37b3
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 13:32:32 -0500

    reseeded data for more info/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-character = Character.create(name: 'Jester')
+character = Character.create(name: 'Jester Lavorre')
+character = Character.create(name: 'Fjord')
+character = Character.create(name: 'Caleb Widogast')
 
 bag = Bag.create(character_id: 1, title: 'lollipop bag', description: 'sweet')
+bag = Bag.create(character_id: 1, title: 'plain backpack', description: 'normal')
+
+bag = Bag.create(character_id: 2, title: 'water-logged backpack', description: 'smells like sea water')
+bag = Bag.create(character_id: 3, title: 'leather satchel', description: 'covered in soot')
