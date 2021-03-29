6f7c5e5820a56f7b83326eef471f0fa7f774ee4f
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-29 12:03:33 -0400

    added code to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+
 end
